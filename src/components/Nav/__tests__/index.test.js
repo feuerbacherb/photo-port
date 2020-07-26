b22d78c5bbd70cad72a833e476208f51db222f9d
Brent Feuerbacher <feuerbacherb@gmail.com>
2020-07-26 12:05:37 -0500

    ran tests to make sure things worked

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    // baseline test
+    it('renders', () => {
+        render(<Nav />);
+    });
+
+    // snapshot test
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+})
+
+
+// test to see if the emoji is visible
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
+  
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+})  
+
+// test to see if the links are visible
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        const { getByTestId } = render(<Nav />);
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About me');
+    });
+});
